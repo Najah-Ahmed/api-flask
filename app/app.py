1bfd6d0038d86784ec68e9a4b25fec84651b25a3
Najah-Ahmed <najaah8800@gmail.com>
2020-06-18 22:56:16 +0300

    by using database sql

--- app/app.py
+++ app/app.py
@@ -1,62 +1,23 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 
 from securty import authenticate, identity
+
+from user import UserRegister
+
+from item import Item, ItemList
 app = Flask(__name__)
 app.secret_key = '889888'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'price', type=float, required=True, help="This field is required"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': 'An item name {} already exist.'.format(name)}, 400
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-
-        data = Item.parser.parse_args()
-        item = next(
-            filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return{'items': items}
-
 
 api.add_resource(Item, '/api/item/<string:name>')
 api.add_resource(ItemList, '/api/items')
+api.add_resource(UserRegister, '/api/register')
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
