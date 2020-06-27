d7d8d63d81fce1a55f8c9c1f75a4e0e3d18e5cfb
Najah-Ahmed <najaah8800@gmail.com>
2020-06-27 12:45:29 +0300

    using sql querys before sqlalchmeny or ORM

--- app/app.py
+++ app/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 
+
 from securty import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
 
-from user import UserRegister
-
-from item import Item, ItemList
 app = Flask(__name__)
 app.secret_key = '889888'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -20,4 +21,6 @@ api.add_resource(UserRegister, '/api/register')
 
 
 if __name__ == "__main__":
+    from database import db
+    db.init_app(app)
     app.run(debug=True)
